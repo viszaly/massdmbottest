41804ae1ec97bf43f9d0d34b307c55f5876618e6
viszaly <71315111+viszaly@users.noreply.github.com>
2021-03-23 19:01:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+
+client.on("message", message => {
+
+if (message.content.startsWith(prefix + "dm")) {
+if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` **MESSAGE SENT** `); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready',()=>{
+    console.log(' Bot is online now!');
+    let statuses = [
+        " INVADER IS OP ",
+        " Join Server - https://discord.gg/4ygg8DF ",
+        " New Video - https://youtu.be/9PZ3UfWdi4w " // U can add another
+        
+    ]
+    setInterval(function(){
+            let status = statuses[Math.floor(Math.random() * statuses.length)];
+            client.user.setActivity(status, {type:"WATCHING"})
+    
+        }, 3000) //Seconds to Random
+
+
+
+});
+
+client.login("NjYxNTc1MTczMjExOTQ3MDE4.Xnh7jQ.Zp8Zyvsp2mNH-cVtfccccXP_K4k");
